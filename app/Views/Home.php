9c44fb5e5c65ddce1967f31fe92d710696f8a155
RenzoEspinozaFlores <espinozaflores.rref@gmail.com>
2022-08-27 16:04:13 -0500

    Base del diseño 2

--- app/Views/Home.php
+++ app/Views/Home.php
@@ -215,16 +215,6 @@
 
     <div class="main-panel">
         <div class="content">
-            <div class="panel-header bg-primary-gradient">
-                <div class="page-inner py-5">
-                    <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
-                        <div>
-                            <h2 class="text-white pb-2 fw-bold">Dashboard</h2>
-                            <h5 class="text-white op-7 mb-2">Free Bootstrap 4 Admin Dashboard</h5>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <div class="page-inner mt--5">
 
             </div>
