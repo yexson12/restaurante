e6c157c5e0e92b9106573577c79f76f821fc4dd6
yexson12 <87106802+yexson12@users.noreply.github.com>
2022-09-07 00:34:11 -0500

    ff

--- app/Controllers/Parrillas.php
+++ app/Controllers/Parrillas.php
@@ -9,9 +9,9 @@ class Parrillas extends BaseController
     public function index()
     {
         $bedida = new Bebida();
-        $datos['producto'] = $bedida->where("ID_CATEGORIA",2)->orderBy('PRODUCTO_ID', 'ASC')->findAll();
+        $datos['producto'] = $bedida->where("ID_CATEGORIA",1)->orderBy('PRODUCTO_ID', 'ASC')->findAll();
         $subcategorias = new Subcategoria();
-        $datos['subcategoria'] = $subcategorias->where("ID_CATEGORIA",2)->orderBy('ID_SUBCAT', 'ASC')->findAll();
+        $datos['subcategoria'] = $subcategorias->where("ID_CATEGORIA",1)->orderBy('ID_SUBCAT', 'ASC')->findAll();
         echo view('header');
         echo view('parrillas',$datos);
         echo view('footer');
--- app/Controllers/Postres.php
+++ app/Controllers/Postres.php
@@ -8,9 +8,9 @@ class Postres extends BaseController
 {
     public function index()
     {      $bedida = new Bebida();
-        $datos['producto'] = $bedida->where("ID_CATEGORIA",1)->orderBy('PRODUCTO_ID', 'ASC')->findAll();
+        $datos['producto'] = $bedida->where("ID_CATEGORIA",4)->orderBy('PRODUCTO_ID', 'ASC')->findAll();
         $subcategorias = new Subcategoria();
-        $datos['subcategoria'] = $subcategorias->where("ID_CATEGORIA",1)->orderBy('ID_SUBCAT', 'ASC')->findAll();
+        $datos['subcategoria'] = $subcategorias->where("ID_CATEGORIA",4)->orderBy('ID_SUBCAT', 'ASC')->findAll();
         echo view('header');
         echo view('postres', $datos);
         echo view('footer');
