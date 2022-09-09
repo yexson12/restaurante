2db48ab27de81ba196994dc24ad211b039a7d772
yexson12 <87106802+yexson12@users.noreply.github.com>
2022-09-09 01:35:25 -0500

    g

--- app/Views/admin/subcategoria/editar_subcat.php
+++ app/Views/admin/subcategoria/editar_subcat.php
@@ -25,7 +25,7 @@
                 </div>
 
 
-<<<<<<< HEAD
+
                 <button class="btn btn-success" type="submit">guardar</button>
             </form>
             </P>
@@ -34,9 +34,8 @@
 
 
 
-=======
+
     </div>
->>>>>>> 67d8756ca396458a0258a3422f63ead94169fb61
 
 
 
