c4c44b86e6d02dcc4db82f8bc5c5ebc5cba53a9b
yexson12 <87106802+yexson12@users.noreply.github.com>
2022-09-09 01:03:30 -0500

    hola

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -59,15 +59,14 @@ $routes->post('combo', 'Combos::combo');
        
 /*ADMINISTRADOR CATEGORIA*/
 $routes->get('admin_cat/', 'Categorias::admin_cat');
-<<<<<<< HEAD
-=======
+
+
 $routes->get('crear_cat/', 'Categorias::crear_cat');
 $routes->get('borrar_cat/(:num)', 'Categorias::borrar_cat/$1');
 $routes->get('editar_cat/(:num)', 'Categorias::editar_cat/$1');
 $routes->post('guardar_cat', 'Categorias::guardar_cat');
 $routes->post('actualizar_cat', 'Categorias::actualizar_cat');
 
->>>>>>> dac45de58832042992fafdc6c655362d4ca127f4
 
 /* FIN ADMINISTRADOR CATEGORIA*/
 /*ADMINISTRADOR SUBCATEGORIA*/
